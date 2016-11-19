adbfea325f07c07b86adcf8c92fd02c948dcc0ec
Tomasz Ożański <tozanski@users.noreply.github.com>
2016-11-19 22:10:15 +0100

    appveyor unit testing attempt #14

--- core/inc/object_tracking/uni_trackee.hpp
+++ core/inc/object_tracking/uni_trackee.hpp
@@ -8,35 +8,37 @@ namespace simbad
 {
 namespace core
 {
-
 template <class Callback, class Derived>
 class uni_trackee : public trackee<tracking_listener<Callback>, Derived>
 {
-  public:
-    using callback_type = Callback;
-    using my_listener = tracking_listener<callback_type>;
-    using my_base = trackee<my_listener, Derived>;
-    //uni_trackee() = default;
-    explicit uni_trackee(Callback c=Callback()) : my_base(my_listener(std::move(c))) {}
+public:
+  using callback_type = Callback;
+  using my_listener = tracking_listener<callback_type>;
+  using my_base = trackee<my_listener, Derived>;
+  // uni_trackee() = default;
+  explicit uni_trackee(Callback c = Callback())
+      : my_base(my_listener(std::move(c)))
+  {
+  }
 
-    uni_trackee(uni_trackee &&) = default;
-    uni_trackee(uni_trackee const &) = delete;
+  uni_trackee(uni_trackee &&) = default;
+  uni_trackee(uni_trackee const &) = delete;
 
-    uni_trackee &operator=(uni_trackee &&) = default;
-    uni_trackee &operator=(uni_trackee const &) = delete;
+  uni_trackee &operator=(uni_trackee &&) = default;
+  uni_trackee &operator=(uni_trackee const &) = delete;
 
-    void set_callback(callback_type callback)
-    {
-        my_base::set_listener(my_listener(std::move(callback)));
-    }
-    callback_type &get_callback()
-    {
-        return my_base::get_listener().get_callback();
-    }
-    callback_type const &get_callback() const
-    {
-        return my_base::get_listener().get_callback();
-    }
+  void set_callback(callback_type callback)
+  {
+    my_base::set_listener(my_listener(std::move(callback)));
+  }
+  callback_type &get_callback()
+  {
+    return my_base::get_listener().get_callback();
+  }
+  callback_type const &get_callback() const
+  {
+    return my_base::get_listener().get_callback();
+  }
 };
 }
 }
