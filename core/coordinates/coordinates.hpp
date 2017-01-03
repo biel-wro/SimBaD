82e0da20aa13a52fe6ba8899f378b968d28b45f7
Tomasz Ożański <tozanski@users.noreply.github.com>
2017-01-03 21:03:34 +0100

    ready to merge #4

--- core/coordinates/coordinates.hpp
+++ core/coordinates/coordinates.hpp
@@ -392,7 +392,7 @@ struct hash<simbad::core::coordinates<Scalar, dimension>>
     return boost::hash_range(c.begin(), c.end());
   }
 };
-
+/*
 template <class Scalar, size_t dimension>
 ::simbad::core::coordinates<Scalar, dimension>
 min(::simbad::core::coordinates<Scalar, dimension> const &c1,
@@ -412,7 +412,7 @@ max(::simbad::core::coordinates<Scalar, dimension> const &c1,
   for(size_t d = 0; d < dimension; ++d)
     ret[d] = std::max(c1[d], c2[d]);
   return ret;
-}
+}*/
 }
 
 #endif
