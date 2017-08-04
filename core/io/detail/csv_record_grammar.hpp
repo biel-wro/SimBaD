93bb4c5f65619c7320ae0287ed0e07a24e085c38
Tomasz Ożański <tozanski@users.noreply.github.com>
2017-08-04 11:07:09 +0200

    compilation fix #2 unsupported header

--- core/io/detail/csv_record_grammar.hpp
+++ core/io/detail/csv_record_grammar.hpp
@@ -5,20 +5,20 @@
 #include "interface/attribute.hpp"
 #include "interface/attribute_description.hpp"
 
-#include <boost/compressed_pair.hpp>
+//#include <boost/compressed_pair.hpp>
 #include <boost/config/warning_disable.hpp>
-#include <boost/fusion/include/adapt_struct.hpp>
+//#include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/at_c.hpp>
-#include <boost/fusion/include/io.hpp>
-#include <boost/fusion/include/std_pair.hpp>
-#include <boost/fusion/include/std_tuple.hpp>
-#include <boost/fusion/sequence/intrinsic/at_c.hpp>
+//#include <boost/fusion/include/io.hpp>
+//#include <boost/fusion/include/std_pair.hpp>
+//#include <boost/fusion/include/std_tuple.hpp>
+//#include <boost/fusion/sequence/intrinsic/at_c.hpp>
 #include <boost/fusion/tuple.hpp>
 #include <boost/phoenix/fusion.hpp>
 #include <boost/phoenix/fusion/at.hpp>
 #include <boost/phoenix/stl.hpp>
 #include <boost/phoenix/stl/container/container.hpp>
-#include <boost/spirit/include/phoenix_bind.hpp>
+//#include <boost/spirit/include/phoenix_bind.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
