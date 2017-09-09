5813edb8007477b77a315e962caea0cee3ddea2b
Tomasz Ożański <tozanski@users.noreply.github.com>
2017-09-09 23:55:21 +0200

    finding error

--- core/processors/dataframe_tracker.cpp
+++ core/processors/dataframe_tracker.cpp
@@ -1,6 +1,7 @@
 #include "dataframe_tracker.hpp"
 
 #include <boost/intrusive/unordered_set.hpp>
+#include <boost/optional.hpp>
 
 #include "interface/attribute.hpp"
 #include "utils/attribute_exceptions.hpp"
@@ -10,7 +11,26 @@ BEGIN_NAMESPACE_CORE
 
 struct dataframe_tracker::node : public dataframe_tracker::hook_type
 {
-  attribute const & get_attribute(std::size_t idx) const
+  static std::vector<attribute>
+  make_attributes(attribute_list const &other_attributes,
+                  std::vector<std::size_t> const &mapping)
+  {
+    std::size_t const size = mapping.size();
+    std::vector<attribute> attributes(size);
+    for(std::size_t my_idx = 0; my_idx < size; ++my_idx)
+    {
+      std::size_t other_idx = mapping[my_idx];
+      attributes[my_idx] = other_attributes[other_idx];
+    }
+    return attributes;
+  }
+
+  node(attribute_list const &other_attributes,
+       std::vector<std::size_t> const &mapping)
+      : m_attributes(make_attributes(other_attributes, mapping))
+  {
+  }
+  attribute const &get_attribute(std::size_t idx) const
   {
     assert(m_attributes.size() > idx);
     return m_attributes[idx];
@@ -27,7 +47,15 @@ operator()(const dataframe_tracker::node &n) const
   return seed;
 }
 
-std::size_t dataframe_tracker::node_equal::
+std::size_t dataframe_tracker::node_hash::
+operator()(attribute const &attr) const
+{
+  std::size_t seed = 0;
+  boost::hash_combine(seed, attr);
+  return seed;
+}
+
+bool dataframe_tracker::node_equal::
 operator()(const dataframe_tracker::node &lhs,
            const dataframe_tracker::node &rhs) const
 {
@@ -35,6 +63,24 @@ operator()(const dataframe_tracker::node &lhs,
     if(lhs.get_attribute(idx) != rhs.get_attribute(idx))
       return false;
   return true;
+}
+
+bool dataframe_tracker::node_equal::
+operator()(dataframe_tracker::node const &node, attribute const &attr) const
+{
+  assert(1 == m_key_size);
+  return node.get_attribute(0) == attr;
+}
+bool dataframe_tracker::node_equal::
+operator()(attribute const &attr, dataframe_tracker::node const &node) const
+{
+  return operator()(node, attr);
+}
+
+bool dataframe_tracker::node_equal::operator()(const attribute &lhs,
+                                               const attribute &rhs) const
+{
+  return lhs == rhs;
 }
 
 dataframe_tracker::dataframe_tracker(attribute_description const &description,
@@ -51,6 +97,21 @@ dataframe_tracker::dataframe_tracker(attribute_description const &description,
   m_attribute_description.add_attributes(description, val_names);
 }
 
-void dataframe_tracker::feed_record(const attribute_list &input) {}
+dataframe_tracker::node_hash dataframe_tracker::hasher() const
+{
+  return m_attribute_set.hash_function();
+}
+dataframe_tracker::node_equal dataframe_tracker::equaler() const
+{
+  return m_attribute_set.key_eq();
+}
+boost::optional<dataframe_tracker::const_iterator>
+dataframe_tracker::find(const attribute &attr) const
+{
+  const_iterator it = m_attribute_set.find(attr, hasher(), equaler());
+  if(it == m_attribute_set.end())
+    return boost::none;
+  return it;
+}
 
 END_NAMESPACE_CORE
--- core/processors/dataframe_tracker.hpp
+++ core/processors/dataframe_tracker.hpp
@@ -5,6 +5,7 @@
 #include "interface/attribute_description.hpp"
 
 #include <boost/intrusive/unordered_set.hpp>
+#include <boost/optional/optional_fwd.hpp>
 
 #include <memory>
 
@@ -12,14 +13,6 @@ BEGIN_NAMESPACE_CORE
 
 class dataframe_tracker
 {
-public:
-  static constexpr std::size_t initial_bucket_count = 1024;
-  dataframe_tracker(attribute_description const &description,
-                    std::vector<std::string> const &key_names,
-                    std::vector<std::string> const &val_names);
-
-  void feed_record(attribute_list const &input);
-
 private:
   using hook_type = boost::intrusive::unordered_set_base_hook<
       boost::intrusive::store_hash<true>>;
@@ -29,12 +22,18 @@ private:
   {
     std::size_t const m_key_size;
     std::size_t operator()(dataframe_tracker::node const &n) const;
+    std::size_t operator()(attribute const &attr) const;
   };
   struct node_equal
   {
     std::size_t const &m_key_size;
-    std::size_t operator()(dataframe_tracker::node const &lhs,
-                           dataframe_tracker::node const &rhs) const;
+    bool operator()(dataframe_tracker::node const &lhs,
+                    dataframe_tracker::node const &rhs) const;
+    bool operator()(dataframe_tracker::node const &node,
+                    attribute const &attr) const;
+    bool operator()(attribute const &attr,
+                    dataframe_tracker::node const &node ) const;
+    bool operator()(attribute const &lhs, attribute const &rhs) const;
   };
 
   using set_type =
@@ -44,11 +43,27 @@ private:
                                       boost::intrusive::compare_hash<true>,   //
                                       boost::intrusive::power_2_buckets<true> //
                                       >;
+
   using bucket_type = set_type::bucket_type;
   std::unique_ptr<bucket_type[]> m_buckets;
   set_type m_attribute_set;
   attribute_description m_attribute_description;
   std::vector<std::size_t> const m_to_outer_indices;
+
+public:
+  using iterator = set_type::iterator;
+  using const_iterator = set_type::const_iterator;
+  using insert_commit_data = set_type::insert_commit_data;
+
+  static constexpr std::size_t initial_bucket_count = 1024;
+  dataframe_tracker(attribute_description const &description,
+                    std::vector<std::string> const &key_names,
+                    std::vector<std::string> const &val_names);
+  node_hash hasher() const;
+  node_equal equaler() const;
+
+  boost::optional<dataframe_tracker::const_iterator>
+  find(attribute const &attr) const;
 };
 END_NAMESPACE_CORE
 
