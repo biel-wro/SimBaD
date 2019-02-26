0f6e8a71ce3eb1a0f9909d0a1f31099e249c956e
Tomasz Ożański <tozanski@users.noreply.github.com>
2019-02-26 23:25:44 +0100

    added segment_multipled time dependence

--- src/core/computational/time_dependent_scalars/repository.cpp
+++ src/core/computational/time_dependent_scalars/repository.cpp
@@ -1,6 +1,7 @@
 #include "repositories/factory_registration.hpp"
 
 #include "identity.hpp"
+#include "segment_multiplied.hpp"
 
 BEGIN_NAMESPACE_CORE
 
@@ -8,6 +9,7 @@ SIMBAD_MAKE_REPOSITORY_EXPLICIT_SPECIALIZATION_DECL(time_dependent_scalar)
 {
   factory<time_dependent_scalar> factory;
   register_creator<identity>(factory, "identity");
+  register_creator<segment_multiplied>(factory, "segment_multiplied");
   return factory;
 }
 
--- src/core/computational/time_dependent_scalars/time_dependent_scalar.hpp
+++ src/core/computational/time_dependent_scalars/time_dependent_scalar.hpp
@@ -9,7 +9,7 @@ class time_dependent_scalar
 {
 public:
   using real_type = double;
-  virtual double operator()(real_type time, real_type value) const = 0;
+  virtual real_type operator()(real_type time, real_type value) const = 0;
   virtual ~time_dependent_scalar();
 };
 
